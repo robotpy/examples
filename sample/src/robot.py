f506d7c91c2b6d09d0ad9c82c6b89ad3d663f83f
Dustin Spicuzza <dustin@virtualroadside.com>
2016-12-17 23:01:03 -0500

    Move RobotPy WPILib examples to this repo

--- sample/src/robot.py
+++ sample/src/robot.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+'''
+    The SampleRobot class is the base of a robot application that will
+    automatically call your Autonomous and OperatorControl methods at
+    the right time as controlled by the switches on the driver station
+    or the field controls.
+    
+    WARNING: While it may look like a good choice to use for your code
+    if you're inexperienced, don't. Unless you know what you are doing,
+    complex code will be much more difficult under this system. Use
+    IterativeRobot or Command-Based instead if you're new.
+'''
+
 
 import wpilib
 
