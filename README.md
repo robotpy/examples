cda0287113997910d6130018b9b1dfe3ef35ae59
Dustin Spicuzza <dustin@virtualroadside.com>
2016-12-30 23:15:09 -0500

    Add travis-ci testing configuration

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 RobotPy Example Code
 ====================
 
+[![Build Status](https://travis-ci.org/robotpy/examples.svg?branch=master)](https://travis-ci.org/robotpy/examples)
+
 This repository contains lots of example code that can be used to learn more
 about creating robots using RobotPy technologies.
 
---
+++ run_tests.sh
@@ -0,0 +1,70 @@
+#!/bin/bash -e
+
+cd "$(dirname $0)"
+
+BASE_TESTS="
+  gearsbot
+  getting-started
+  gyro
+  iterative/src
+  mecanum-drive
+  motor-control
+  pacgoat
+  physics/src
+  physics-4wheel/src
+  physics-mecanum/src
+  physics-spi/src
+  sample/src
+  tank-drive
+"
+
+ROBOTPY_EXT_TESTS="
+  command-based
+  navx
+  navx-rotate-to-angle
+  navx-rotate-to-angle-arcade
+  magicbot-simple
+  stateful-autonomous
+"
+
+IGNORED_TESTS=
+
+ALL_TESTS="${BASE_TESTS} ${ROBOTPY_EXT_TESTS}"
+EVERY_TESTS="${ALL_TESTS} ${IGNORED_TESTS}"
+
+if [ "$1" == "all" ]; then
+  TESTS="$ALL_TESTS"
+elif [ "$1" == "base" ]; then
+  TESTS="$BASE_TESTS"
+else
+  echo "Usage: run_tests.sh all|base"
+  exit 1
+fi
+
+# Ensure that when new samples are added, they are added to the list of things
+# to test. Otherwise, exit.
+EVERY_TESTS=$(for i in ${EVERY_TESTS}; do
+  echo ./$i/robot.py
+done | sort)
+
+FOUND_TESTS=$(find -name robot.py | sort)
+
+if [ "$EVERY_TESTS" != "$FOUND_TESTS" ]; then
+  echo "Specified:"
+  echo "$EVERY_TESTS"
+  echo
+  echo "Found:"
+  echo "$FOUND_TESTS"
+  echo
+  echo "ERROR: Not every robot.py file is in the list of tests!"
+  exit 1
+fi
+
+for t in ${TESTS}; do
+  pushd $t > /dev/null
+  pwd
+  python3 robot.py test --builtin
+  popd > /dev/null
+done
+
+echo "All tests successful!"
