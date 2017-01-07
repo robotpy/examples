cf02dfb85dc5e7b1e8b01ca21a8bea8a75815316
Dustin Spicuzza <dustin@virtualroadside.com>
2017-01-06 23:29:28 -0500

    Add remote testing script

---
+++ _remote_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# Checks out repo 
+
+if [ -d examples ]; then
+    cd examples
+    git fetch --depth=1
+    git checkout origin/master
+else
+    git clone --depth=1 https://github.com/robotpy/examples
+    cd examples
+fi
+
+exec ./run_tests.sh "$@"
