5979ff4ca3368e2e08ba4a358efc545bd1f719ae
Dustin Spicuzza <dustin@virtualroadside.com>
2017-03-04 13:20:28 -0500

    Update magicbot example to use magicbot state machines

--- magicbot-simple/autonomous/two_steps.py
+++ magicbot-simple/autonomous/two_steps.py
@@ -1,20 +1,16 @@
 
-from robotpy_ext.autonomous import StatefulAutonomous, timed_state
+from magicbot import AutonomousStateMachine, tunable, timed_state
              
 from components.component2 import Component2
                     
-class TwoSteps(StatefulAutonomous):
+class TwoSteps(AutonomousStateMachine):
 
     MODE_NAME = 'Two Steps'
     DEFAULT = True
     
     component2 = Component2
-
-    def initialize(self):
-        
-        # This allows you to tune the variable via the SmartDashboard over
-        # networktables
-        self.register_sd_var('drive_speed', -1)
+    
+    drive_speed = tunable(-1)
 
     @timed_state(duration=2, next_state='do_something', first=True)
     def dont_do_something(self):
