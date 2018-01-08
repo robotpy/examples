f4aa87c888f3b8248404d7887be954f16b1275b4
Benjamin Ward <ward.programm3r@gmail.com>
2018-01-08 16:16:50 -0500

    Update getting-started example to use Timer as per https://github.com/wpilibsuite/EclipsePlugins/commit/f4131c91ac01cd21bac36c8372d0ab7046d3c4b2#diff-c0b3b6a7b702a9920b95516dac2c0c7c

--- getting-started/robot.py
+++ getting-started/robot.py
@@ -18,28 +18,25 @@ class MyRobot(wpilib.IterativeRobot):
         self.right_motor = wpilib.Spark(1)
         self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
         self.stick = wpilib.Joystick(1)
+        self.timer = wpilib.Timer()
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        self.auto_loop_counter = 0
+        self.timer.reset()
+        self.timer.start()
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
 
-        # Check if we've completed 100 loops (approximately 2 seconds)
-        if self.auto_loop_counter < 100:
+        # Drive for two seconds
+        if self.timer.get() < 2.0:
             self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-            self.auto_loop_counter += 1
         else:
             self.drive.arcadeDrive(0, 0)  # Stop robot
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
-
-    def testPeriodic(self):
-        """This function is called periodically during test mode."""
-        pass
 
 
 if __name__ == "__main__":
