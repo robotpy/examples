e5fd48f4f975c59cd6f28e3988d9ef2b5e1c25dc
Dustin Spicuzza <dustin@virtualroadside.com>
2018-04-20 02:29:36 -0400

    Upgrade to use the realistic tankmodel for physics

--- physics-4wheel/src/physics.py
+++ physics-4wheel/src/physics.py
@@ -3,12 +3,13 @@
 #
 
 
-from pyfrc.physics import drivetrains
+from pyfrc.physics import motor_cfgs, tankmodel
+from pyfrc.physics.units import units
 
 
 class PhysicsEngine(object):
     '''
-       Simulates a 4-wheel robot using Tank Drive joystick control 
+       Simulates a 4-wheel robot using Tank Drive joystick control
     '''
     
     
@@ -19,7 +20,27 @@ class PhysicsEngine(object):
         '''
         
         self.physics_controller = physics_controller
-    
+        
+        # Change these parameters to fit your robot!
+        motor_cfg = motor_cfgs.MOTOR_CFG_CIM
+        robot_mass = 110*units.lbs
+        
+        bumper_width = 3.25*units.inch
+        robot_wheelbase = 22*units.inch
+        robot_width = 23*units.inch + bumper_width*2
+        robot_length = 32*units.inch + bumper_width*2
+        
+        wheel_diameter = 6*units.inch
+        drivetrain_gear_ratio = 10.71
+        motors_per_side = 2
+        
+        self.drivetrain = tankmodel.TankModel.theory(motor_cfg,
+                                                     robot_mass, drivetrain_gear_ratio, motors_per_side,
+                                                     robot_wheelbase,
+                                                     robot_width,
+                                                     robot_length,
+                                                     wheel_diameter)
+
     def update_sim(self, hal_data, now, tm_diff):
         '''
             Called when the simulation parameters for the program need to be
@@ -33,8 +54,10 @@ class PhysicsEngine(object):
         # Simulate the drivetrain
         lr_motor = hal_data['pwm'][1]['value']
         rr_motor = hal_data['pwm'][2]['value']
-        lf_motor = hal_data['pwm'][3]['value']
-        rf_motor = hal_data['pwm'][4]['value']
         
-        speed, rotation = drivetrains.four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
-        self.physics_controller.drive(speed, rotation, tm_diff)
+        # Not needed because front and rear should be in sync
+        #lf_motor = hal_data['pwm'][3]['value']
+        #rf_motor = hal_data['pwm'][4]['value']
+        
+        x, y, angle = self.drivetrain.get_distance(lr_motor, rr_motor, tm_diff)
+        self.physics_controller.distance_drive(x, y, angle)
