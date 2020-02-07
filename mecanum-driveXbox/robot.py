c5006f97253e61c0827e5f031d6ee334dec2b0cf
leko <lekofraggle42@gmail.com>
2020-02-07 14:34:54 -0500

    Added Xbox Mecanum Drive Example

---
+++ mecanum-driveXbox/robot.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+    This is a demo program showing how to use Mecanum control with the
+    MecanumDrive class.
+"""
+
+import wpilib
+from wpilib.drive import MecanumDrive
+#Import the Xbox specific control
+from wpilib.interfaces import GenericHID
+
+Hand = GenericHID.Hand
+
+class MyRobot(wpilib.TimedRobot):
+    # Channels on the roboRIO that the motor controllers are plugged in to
+    frontLeftChannel = 2
+    rearLeftChannel = 3
+    frontRightChannel = 1
+    rearRightChannel = 0
+
+    # The channel on the driver station that the joystick is connected to
+    joystickChannel = 0
+    
+
+    def robotInit(self):
+        """Robot initialization function"""
+        self.frontLeftMotor = wpilib.Talon(self.frontLeftChannel)
+        self.rearLeftMotor = wpilib.Talon(self.rearLeftChannel)
+        self.frontRightMotor = wpilib.Talon(self.frontRightChannel)
+        self.rearRightMotor = wpilib.Talon(self.rearRightChannel)
+
+        # invert the left side motors
+        self.frontLeftMotor.setInverted(True)
+
+        # you may need to change or remove this to match your robot
+        self.rearLeftMotor.setInverted(True)
+
+        self.drive = MecanumDrive(
+            self.frontLeftMotor,
+            self.rearLeftMotor,
+            self.frontRightMotor,
+            self.rearRightMotor,
+        )
+
+        self.drive.setExpiration(0.1)
+
+        # Define the Xbox Controller.
+        self.stick = wpilib.XboxController(self.joystickChannel)
+
+    def teleopInit(self):
+        self.drive.setSafetyEnabled(True)
+
+    def teleopPeriodic(self):
+        """Runs the motors with Mecanum drive."""
+        # Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+        # This sample does not use field-oriented drive, so the gyro input is set to zero.
+        # This Stick configuration is created by K.E. on our team.  Left stick Y axis is speed, Left Stick X axis is strafe, and Right Stick Y axis is turn.
+        self.drive.driveCartesian(
+            self.stick.getX(self.stick.Hand.kLeftHand), self.stick.getY(self.stick.Hand.kLeftHand), self.stick.getY(self.stick.Hand.kRightHand), 0
+        )
+
+        """Alternatively, to match the driver station enumeration, you may use  ---> self.drive.driveCartesian(
+            self.stick.getRawAxis(1), self.stick.getRawAxis(3), self.stick.getRawAxis(2), 0
+        )"""
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
