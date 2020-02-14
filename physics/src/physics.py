88cf58fd0e81b5003fcadafa39a86d897214ab81
Dustin Spicuzza <dustin@virtualroadside.com>
2020-02-14 03:02:01 -0500

    Update physics example for 2020
    - It's hacky, I know...

--- physics/src/physics.py
+++ physics/src/physics.py
@@ -1,4 +1,7 @@
 #
+# TODO: This example has been updated for 2020, but still needs
+#       quite a bit of polish
+#
 # See the documentation for more details on how this works
 #
 # The idea here is you provide a simulation object that overrides specific
@@ -9,9 +12,51 @@
 # of your robot code without too much extra effort.
 #
 
+import math
+import hal.simulation
+
 
 from pyfrc.physics import motor_cfgs, tankmodel
 from pyfrc.physics.units import units
+
+
+class Field:
+    # TODO: this will be in a future pyfrc release
+
+    def __init__(self):
+        self.device = hal.SimDevice("Field2D")
+        self.fx = self.device.createDouble("x", False, 0.0)
+        self.fy = self.device.createDouble("y", False, 0.0)
+        self.frot = self.device.createDouble("rot", False, 0.0)
+
+        self.x = 0
+        self.y = 0
+        self.angle = 0
+
+    def distance_drive(self, x, y, angle):
+        """Call this from your :func:`PhysicsEngine.update_sim` function.
+           Will update the robot's position on the simulation field.
+           
+           This moves the robot some relative distance and angle from
+           its current position.
+           
+           :param x:     Feet to move the robot in the x direction
+           :param y:     Feet to move the robot in the y direction
+           :param angle: Radians to turn the robot
+        """
+        # TODO: use wpilib kinematics?
+
+        self.angle += angle
+
+        c = math.cos(self.angle)
+        s = math.sin(self.angle)
+
+        self.x += x * c - y * s
+        self.y += x * s + y * c
+        
+        self.fx.set(self.x)
+        self.fy.set(self.y)
+        self.frot.set(math.degrees(self.angle))
 
 
 class PhysicsEngine(object):
@@ -21,13 +66,20 @@ class PhysicsEngine(object):
         realistic, but it's good enough to illustrate the point
     """
 
-    def __init__(self, physics_controller):
-        """
-            :param physics_controller: `pyfrc.physics.core.PhysicsInterface` object
-                                       to communicate simulation effects to
-        """
+    def __init__(self):
 
-        self.physics_controller = physics_controller
+        self.field = Field()
+
+        # Motors
+        self.l_motor = hal.simulation.PWMSim(1)
+        self.r_motor = hal.simulation.PWMSim(2)
+
+        self.dio1 = hal.simulation.DIOSim(1)
+        self.dio2 = hal.simulation.DIOSim(2)
+        self.ain2 = hal.simulation.AnalogInSim(2)
+
+        self.motor = hal.simulation.PWMSim(4)
+
         self.position = 0
 
         # Change these parameters to fit your robot!
@@ -46,7 +98,7 @@ class PhysicsEngine(object):
         )
         # fmt: on
 
-    def update_sim(self, hal_data, now, tm_diff):
+    def update_sim(self, now, tm_diff):
         """
             Called when the simulation parameters for the program need to be
             updated.
@@ -57,14 +109,14 @@ class PhysicsEngine(object):
         """
 
         # Simulate the drivetrain
-        l_motor = hal_data["pwm"][1]["value"]
-        r_motor = hal_data["pwm"][2]["value"]
+        l_motor = self.l_motor.getSpeed()
+        r_motor = self.r_motor.getSpeed()
 
         x, y, angle = self.drivetrain.get_distance(l_motor, r_motor, tm_diff)
-        self.physics_controller.distance_drive(x, y, angle)
+        self.field.distance_drive(x, y, angle)
 
         # update position (use tm_diff so the rate is constant)
-        self.position += hal_data["pwm"][4]["value"] * tm_diff * 3
+        self.position += self.motor.getSpeed() * tm_diff * 3
 
         # update limit switches based on position
         if self.position <= 0:
@@ -80,6 +132,6 @@ class PhysicsEngine(object):
             switch2 = False
 
         # set values here
-        hal_data["dio"][1]["value"] = switch1
-        hal_data["dio"][2]["value"] = switch2
-        hal_data["analog_in"][2]["voltage"] = self.position
+        self.dio1.setValue(switch1)
+        self.dio2.setValue(switch2)
+        self.ain2.setVoltage(self.position)
