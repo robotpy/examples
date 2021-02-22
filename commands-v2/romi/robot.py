994eee8af3f8fbec175f3eaf7f265ad1f52a7c66
Dustin Spicuzza <dustin@virtualroadside.com>
2021-02-21 23:33:30 -0500

    Add ROMI example

---
+++ commands-v2/romi/robot.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+# Example that shows how to connect to a ROMI from RobotPy
+#
+# Requirements
+# ------------
+#
+# You must have the robotpy-halsim-ws package installed. This is best done via:
+#
+#    # Windows
+#    py -3 -m pip install robotpy[commands2,sim]
+#
+#    # Linux/macOS
+#    pip3 install robotpy[commands2,sim]
+#
+# Run the program
+# ---------------
+#
+# To run the program you will need to explicitly use the ws-client option:
+#
+#    # Windows
+#    py -3 robot.py sim --ws-client
+#
+#    # Linux/macOS
+#    python robot.py sim --ws-client
+#
+# By default the WPILib simulation GUI will be displayed. To disable the display
+# you can add the --nogui option
+#
+
+import os
+import typing
+
+import wpilib
+import commands2
+
+from robotcontainer import RobotContainer
+
+
+class MyRobot(commands2.TimedCommandRobot):
+    """
+    Our default robot class, pass it to wpilib.run
+
+    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
+    has an implementation of robotPeriodic which runs the scheduler for you
+    """
+
+    autonomousCommand: typing.Optional[commands2.Command] = None
+
+    def robotInit(self) -> None:
+        """
+        This function is run when the robot is first started up and should be used for any
+        initialization code.
+        """
+
+        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # autonomous chooser on the dashboard.
+        self.container = RobotContainer()
+
+    def disabledInit(self) -> None:
+        """This function is called once each time the robot enters Disabled mode."""
+
+    def disabledPeriodic(self) -> None:
+        """This function is called periodically when disabled"""
+
+    def autonomousInit(self) -> None:
+        """This autonomous runs the autonomous command selected by your RobotContainer class."""
+        self.autonomousCommand = self.container.getAutonomousCommand()
+
+        if self.autonomousCommand:
+            self.autonomousCommand.schedule()
+
+    def autonomousPeriodic(self) -> None:
+        """This function is called periodically during autonomous"""
+
+    def teleopInit(self) -> None:
+        # This makes sure that the autonomous stops running when
+        # teleop starts running. If you want the autonomous to
+        # continue until interrupted by another command, remove
+        # this line or comment it out.
+        if self.autonomousCommand:
+            self.autonomousCommand.cancel()
+
+    def teleopPeriodic(self) -> None:
+        """This function is called periodically during operator control"""
+
+    def testInit(self) -> None:
+        # Cancels all running commands at the start of test mode
+        commands2.CommandScheduler.getInstance().cancelAll()
+
+
+if __name__ == "__main__":
+
+    # If your ROMI isn't at the default address, set that here
+    os.environ["HALSIMWS_HOST"] = "10.0.0.2"
+    os.environ["HALSIMWS_PORT"] = "3300"
+
+    wpilib.run(MyRobot)
