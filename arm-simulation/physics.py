9ac90d6cd4622b5d958190e418a9c65a64a28eaa
Dustin Spicuzza <dustin@virtualroadside.com>
2022-03-06 00:06:42 -0500

    Add arm simulation example

---
+++ arm-simulation/physics.py
@@ -0,0 +1,104 @@
+#
+# See the documentation for more details on how this works
+#
+# Documentation can be found at https://robotpy.readthedocs.io/projects/pyfrc/en/latest/physics.html
+#
+# The idea here is you provide a simulation object that overrides specific
+# pieces of WPILib, and modifies motors/sensors accordingly depending on the
+# state of the simulation. An example of this would be measuring a motor
+# moving for a set period of time, and then changing a limit switch to turn
+# on after that period of time. This can help you do more complex simulations
+# of your robot code without too much extra effort.
+#
+# Examples can be found at https://github.com/robotpy/examples
+
+import wpilib
+import wpilib.simulation
+
+import wpimath.system.plant
+from pyfrc.physics.core import PhysicsInterface
+
+import math
+import typing
+
+if typing.TYPE_CHECKING:
+    from robot import MyRobot
+
+
+class PhysicsEngine:
+    """
+    Simulates a 4-wheel robot using Tank Drive joystick control
+    """
+
+    def __init__(self, physics_controller: PhysicsInterface, robot: "MyRobot"):
+        """
+        :param physics_controller: `pyfrc.physics.core.Physics` object
+                                   to communicate simulation effects to
+        :param robot: your robot object
+        """
+
+        self.physics_controller = physics_controller
+
+        # The arm gearbox represents a gearbox containing two Vex 775pro motors.
+        self.armGearbox = wpimath.system.plant.DCMotor.vex775Pro(2)
+
+        # Simulation classes help us simulate what's going on, including gravity.
+        # This sim represents an arm with 2 775s, a 600:1 reduction, a mass of 5kg,
+        # 30in overall arm length, range of motion in [-75, 255] degrees, and noise
+        # with a standard deviation of 1 encoder tick.
+        self.armSim = wpilib.simulation.SingleJointedArmSim(
+            self.armGearbox,
+            600.0,
+            wpilib.simulation.SingleJointedArmSim.estimateMOI(0.762, 5),
+            0.762,
+            math.radians(-75),
+            math.radians(255),
+            5,
+            True,
+            [robot.kArmEncoderDistPerPulse],
+        )
+        self.encoderSim = wpilib.simulation.EncoderSim(robot.encoder)
+        self.motorSim = wpilib.simulation.PWMSim(robot.motor.getChannel())
+
+        # Create a Mechanism2d display of an Arm
+        self.mech2d = wpilib.Mechanism2d(60, 60)
+        self.armBase = self.mech2d.getRoot("ArmBase", 30, 30)
+        self.armTower = self.armBase.appendLigament(
+            "Arm Tower", 30, -90, 6, wpilib.Color8Bit(wpilib.Color.kBlue)
+        )
+        self.arm = self.armBase.appendLigament(
+            "Arm", 30, self.armSim.getAngle(), 6, wpilib.Color8Bit(wpilib.Color.kYellow)
+        )
+
+        # Put Mechanism to SmartDashboard
+        wpilib.SmartDashboard.putData("Arm Sim", self.mech2d)
+
+    def update_sim(self, now: float, tm_diff: float) -> None:
+        """
+        Called when the simulation parameters for the program need to be
+        updated.
+
+        :param now: The current time as a float
+        :param tm_diff: The amount of time that has passed since the last
+                        time that this function was called
+        """
+
+        # First, we set our "inputs" (voltages)
+        self.armSim.setInput(
+            0, self.motorSim.getSpeed() * wpilib.RobotController.getInputVoltage()
+        )
+
+        # Next, we update it
+        self.armSim.update(tm_diff)
+
+        # Finally, we set our simulated encoder's readings and simulated battery
+        # voltage
+        self.encoderSim.setDistance(self.armSim.getAngle())
+        # SimBattery estimates loaded battery voltage
+        # wpilib.simulation.RoboRioSim.setVInVoltage(
+        #     wpilib.simulation.BatterySim
+        # )
+
+        # Update the mechanism arm angle based on the simulated arm angle
+        # -> setAngle takes degrees, getAngle returns radians... >_>
+        self.arm.setAngle(math.degrees(self.armSim.getAngle()))
---
+++ arm-simulation/robot.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import math
+import wpilib
+import wpimath.controller
+
+
+class MyRobot(wpilib.TimedRobot):
+    kMotorPort = 0
+    kEncoderAChannel = 0
+    kEncoderBChannel = 1
+    kJoystickPort = 0
+
+    kArmPositionKey = "ArmPosition"
+    kArmPKey = "ArmP"
+
+    # distance per pulse = (angle per revolution) / (pulses per revolution)
+    #  = (2 * PI rads) / (4096 pulses)
+    kArmEncoderDistPerPulse = 2.0 * math.pi / 4096.0
+
+    def robotInit(self) -> None:
+        # The P gain for the PID controller that drives this arm.
+        self.kArmKp = 50.0
+
+        self.armPosition = 75.0
+
+        # standard classes for controlling our arm
+        self.controller = wpimath.controller.PIDController(self.kArmKp, 0, 0)
+        self.encoder = wpilib.Encoder(self.kEncoderAChannel, self.kEncoderBChannel)
+        self.motor = wpilib.PWMSparkMax(self.kMotorPort)
+        self.joystick = wpilib.Joystick(self.kJoystickPort)
+
+        self.encoder.setDistancePerPulse(self.kArmEncoderDistPerPulse)
+
+        # Set the Arm position setpoint and P constant to Preferences if the keys
+        # don't already exist
+        if not wpilib.Preferences.containsKey(self.kArmPositionKey):
+            wpilib.Preferences.setDouble(self.kArmPositionKey, self.armPosition)
+        if not wpilib.Preferences.containsKey(self.kArmPKey):
+            wpilib.Preferences.setDouble(self.kArmPKey, self.kArmKp)
+
+    def teleopInit(self) -> None:
+        # Read Preferences for Arm setpoint and kP on entering Teleop
+        self.armPosition = wpilib.Preferences.getDouble(
+            self.kArmPositionKey, self.armPosition
+        )
+        if self.kArmKp != wpilib.Preferences.getDouble(self.kArmPKey, self.kArmKp):
+            self.kArmKp = wpilib.Preferences.getDouble(self.kArmPKey, self.kArmKp)
+            self.controller.setP(self.kArmKp)
+
+    def teleopPeriodic(self) -> None:
+        if self.joystick.getTrigger():
+            # Here we run PID control like normal, with a setpoint read from
+            # preferences in degrees
+            pidOutput = self.controller.calculate(
+                self.encoder.getDistance(), math.radians(self.armPosition)
+            )
+            self.motor.setVoltage(pidOutput)
+        else:
+            # Otherwise we disable the motor
+            self.motor.set(0.0)
+
+    def disabledInit(self) -> None:
+        # This just makes sure that our simulation code knows that the motor is off
+        self.motor.set(0)
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
