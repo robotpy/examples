a015b0572d8df20c1528d8f0dbad37ada81bd452
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-11 21:47:04 -0300

    Update ledData to avoid high memory usage

--- addressableled/robot.py
+++ addressableled/robot.py
@@ -1,50 +1,51 @@
-from wpilib import run, TimedRobot, AddressableLED
+import wpilib
 
 kLEDBuffer = 60
 
-class MyRobot(TimedRobot):
+
+class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         # PWM Port 9
         # Must be a PWM header, not MXP or DIO
-        self.m_led = AddressableLED(9)
-        
+        self.led = wpilib.AddressableLED(9)
+
         # LED Data
-        self.m_ledData = []
+        self.ledData = [wpilib.AddressableLED.LEDData() for i in range(kLEDBuffer)]
 
         # Store what the last hue of the first pixel is
-        self.m_rainbowFirstPixelHue = 0
+        self.rainbowFirstPixelHue = 0
 
         # Default to a length of 60, start empty output
         # Length is expensive to set, so only set it once, then just update data
-        self.m_led.setLength(kLEDBuffer)
-        
-        self.m_led.start()
+        self.led.setLength(kLEDBuffer)
+
+        # Set the data
+        self.led.setData(self.ledData)
+        self.led.start()
 
     def robotPeriodic(self):
         # Fill the buffer with a rainbow
         self.rainbow()
-        
+
         # Set the LEDs
-        if len(self.m_ledData) <= kLEDBuffer:
-            self.m_led.setData(self.m_ledData)
+        self.led.setData(self.ledData)
 
     def rainbow(self):
         # For every pixel
         for i in range(kLEDBuffer):
             # Calculate the hue - hue is easier for rainbows because the color
             # shape is a circle so only one value needs to precess
-            hue = (self.m_rainbowFirstPixelHue + (i * 180 / kLEDBuffer)) % 180
-            
-            self.m_ledData.append(AddressableLED.LEDData())
-            
+            hue = (self.rainbowFirstPixelHue + (i * 180 / kLEDBuffer)) % 180
+
             # Set the value
-            self.m_ledData[i].setHSV(int(hue), 255, 128)
+            self.ledData[i].setHSV(int(hue), 255, 128)
 
         # Increase by to make the rainbow "move"
-        self.m_rainbowFirstPixelHue += 3
+        self.rainbowFirstPixelHue += 3
 
         # Check bounds
-        self.m_rainbowFirstPixelHue %= 100
+        self.rainbowFirstPixelHue %= 100
 
-if __name__ == '__main__':
-    run(MyRobot)
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
