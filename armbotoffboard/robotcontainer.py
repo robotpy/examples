579da41f29b183dbcce7efbf5fa49706d1656be1
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-21 11:26:14 -0300

    feat: create the robot container file

---
+++ armbotoffboard/robotcontainer.py
@@ -0,0 +1,8 @@
+class RobotContainer:
+    def __init__(self):
+        """
+        This class is where the bulk of the robot should be declared. Since Command-based is a
+        "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
+        periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+        subsystems, commands, and button mappings) should be declared here.
+        """
