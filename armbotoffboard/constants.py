25a141b134bd6f594b42c8b17cdb9c0bc49cc75a
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-21 10:24:44 -0300

    feat: create the constants file

---
+++ armbotoffboard/constants.py
@@ -0,0 +1,15 @@
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+
+"""
+The constants module is a convenience place for teams to hold robot-wide
+numerical or boolean constants. Don't use this for any other purpose!
+"""
+
+# Arm
+kMotorPort = 4
+kSVolts = 1
+kGVolts = 1
+kVVoltSecondPerRad = 0.5
+kAVoltSecondSquaredPerRad = 0.1
