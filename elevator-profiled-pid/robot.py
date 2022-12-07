e47510b51154780fbdcc9c5b9d7abf10f354ecfb
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2022-12-07 22:25:22 +0300

    put everything in robotInit, tests fail otherwise.

--- elevator-profiled-pid/robot.py
+++ elevator-profiled-pid/robot.py
@@ -8,16 +8,16 @@ class MyRobot(wpilib.TimedRobot):
 
     kDt = 0.02
 
-    joystick = wpilib.Joystick(1)
-    encoder = wpilib.Encoder(1, 2)
-    motor = wpilib.PWMSparkMax(1)
-
-    # Create a PID controller whose setpoint's change is subject to maximum
-    # velocity and acceleration constraints.
-    constraints = wpimath.trajectory.TrapezoidProfile.Constraints(1.75, 0.75)
-    controller = wpimath.controller.ProfiledPIDController(1.3, 0, 0.7, constraints, kDt)
-
     def robotInit(self) -> None:
+        self.joystick = wpilib.Joystick(1)
+        self.encoder = wpilib.Encoder(1, 2)
+        self.motor = wpilib.PWMSparkMax(1)
+
+        # Create a PID controller whose setpoint's change is subject to maximum
+        # velocity and acceleration constraints.
+        self.constraints = wpimath.trajectory.TrapezoidProfile.Constraints(1.75, 0.75)
+        self.controller = wpimath.controller.ProfiledPIDController(1.3, 0, 0.7, self.constraints, self.kDt)
+
         self.encoder.setDistancePerPulse(1 / 360 * 2 * math.pi * 1.5)
 
     def teleopPeriodic(self) -> None:
