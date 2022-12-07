8a05ec4d6edff9d2309d58bd64415b7d967b011f
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2022-12-07 22:11:36 +0300

    Add to tests and format with black.

--- elevator-profiled-pid/robot.py
+++ elevator-profiled-pid/robot.py
@@ -3,6 +3,7 @@ import wpimath.controller
 import wpimath.trajectory
 import math
 
+
 class MyRobot(wpilib.TimedRobot):
 
     kDt = 0.02
@@ -18,13 +19,13 @@ class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self) -> None:
         self.encoder.setDistancePerPulse(1 / 360 * 2 * math.pi * 1.5)
-    
+
     def teleopPeriodic(self) -> None:
         if self.joystick.getRawButtonPressed(2):
             self.controller.setGoal(5)
         elif self.joystick.getRawButtonPressed(3):
             self.controller.setGoal(0)
-        
+
         # Run controller and update motor output
         self.motor.set(self.controller.calculate(self.encoder.getDistance()))
 
