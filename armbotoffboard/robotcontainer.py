47af06689497eafc6e5c78f676196682d0534592
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-12 19:02:07 -0300

    style(robotcontainer): class docstring

--- armbotoffboard/robotcontainer.py
+++ armbotoffboard/robotcontainer.py
@@ -10,14 +10,15 @@ from subsystems.armsubsystem import ArmSubsystem
 from subsystems.drivesubsystem import DriveSubsystem
 
 class RobotContainer:
-    def __init__(self) -> None:
-        """
-        This class is where the bulk of the robot should be declared. Since Command-based is a
-        "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
-        periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
-        subsystems, commands, and button mappings) should be declared here.
-        """
+    
+    """
+    This class is where the bulk of the robot should be declared. Since Command-based is a
+    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
+    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+    subsystems, commands, and button mappings) should be declared here.
+    """
 
+    def __init__(self) -> None:
         # The robot's subsystems
         self.robotDrive = DriveSubsystem()
         self.robotArm = ArmSubsystem()
