cec84c7c9a406b0c320d76921dda9ecafa805d2c
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2023-01-08 22:23:22 +0300

    add scheduler-event-logging to tests

--- run_tests.sh
+++ run_tests.sh
@@ -14,6 +14,7 @@ BASE_TESTS="
   commands-v2/hatchbot
   commands-v2/hatchbot-inlined
   commands-v2/ramsete
+  commands-v2/scheduler-event-logging
   commands-v2/selectcommand
   cscore-intermediate-vision
   cscore-quick-vision
