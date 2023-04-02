89202cb4f81534e145ca54997f2fc3c4a3511c24
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2023-04-02 12:13:54 +0300

    initial commit

---
+++ commands-v2/rapid-react-command-bot/robotcontainer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+import commands2
+import commands2.button
+import commands2.cmd
+
+
+class RobotContainer:
+    """
+    This class is where the bulk of the robot should be declared. Since Command-based is a
+    "declarative" paradigm, very little robot logic should actually be handled in the :class:`.Robot`
+    periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+    subsystems, commands, and button mappings) should be declared here.
+    """
+
+    def __init__(self) -> None:
+        self.configureButtonBindings()
+
+    def configureButtonBindings(self):
+        pass
+
+    def getAutonomousCommand(self) -> commands2.Command:
+        return commands2.cmd.nothing()
