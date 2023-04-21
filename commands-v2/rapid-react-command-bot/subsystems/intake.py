22af60164a683ae1a916d750659cb2b44ba60c5b
BerkeSinanYetkin <berkesinanyetkin@gmail.com>
2023-04-21 21:23:22 +0300

    formatting fix

--- commands-v2/rapid-react-command-bot/subsystems/intake.py
+++ commands-v2/rapid-react-command-bot/subsystems/intake.py
@@ -11,31 +11,31 @@ import commands2.cmd
 
 import constants
 
-class Intake(commands2.SubsystemBase):
 
+class Intake(commands2.SubsystemBase):
     def __init__(self) -> None:
         super().__init__()
 
         self.motor = wpilib.PWMSparkMax(constants.IntakeConstants.kMotorPort)
         self.pistons = wpilib.DoubleSolenoid(
-            wpilib.PneumaticsModuleType.REVPH,
-            *constants.IntakeConstants.kSolenoidPorts
+            wpilib.PneumaticsModuleType.REVPH, *constants.IntakeConstants.kSolenoidPorts
         )
-    
+
     # Returns a command that deploys the intake, and then runs the intake motor indefinitely.
     def intakeCommand(self) -> commands2.Command:
         self.motorSetCommand = self.run(lambda: self.motor.set(1))
         self.motorSetCommand.setName("Intake")
         return self.runOnce(
             lambda: self.pistons.set(wpilib.DoubleSolenoid.Value.kForward)
-        ).andThen(
-            self.motorSetCommand
-        )
+        ).andThen(self.motorSetCommand)
 
     # Returns a command that turns off and retracts the intake.
     def retractCommand(self) -> commands2.Command:
         command = self.runOnce(
-            lambda: [self.motor.disable(), self.pistons.set(wpilib.DoubleSolenoid.Value.kReverse)]
+            lambda: [
+                self.motor.disable(),
+                self.pistons.set(wpilib.DoubleSolenoid.Value.kReverse),
+            ]
         )
         command.setName("Retract")
         return command
