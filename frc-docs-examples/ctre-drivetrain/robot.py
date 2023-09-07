4a3e6638e108499fee0addac74fd2f918c45b2d8
Martin Rioux <martin.rioux2@gmail.com>
2023-09-07 08:45:08 -0400

    Added drivetrain demo for rev, PWM motors and ctre

---
+++ frc-docs-examples/ctre-drivetrain/robot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    PWM Drivetrain Demo
+"""
+
+# The first lines should be the import of the needed modules
+import wpilib
+import wpilib.drive
+import ctre
+
+
+# All code should be found in the MyRobot class. Note that in this example, the wpilib.TimedRobot robot base is used.
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        """Robot initialization function"""
+
+        # create motor controller objects
+        m_left = ctre.WPI_TalonFX(1)
+        m_right = ctre.WPI_TalonFX(2)
+        self.robot_drive = wpilib.drive.DifferentialDrive(m_left, m_right)  # object that handles basic drive operations
+        self.robot_drive.setExpiration(0.1)  # Configure protection in case of code slowdown or crash
+
+        # joystick #0
+        self.stick = wpilib.Joystick(0)
+
+    def teleopInit(self):
+        """Executed once at the start of teleop mode"""
+        self.robot_drive.setSafetyEnabled(True)
+
+    def teleopPeriodic(self):
+        """Executed in loop while in teleop mode"""
+
+        # Runs the motors with tank steering
+        self.robot_drive.arcadeDrive(
+            self.stick.getRawAxis(0), self.stick.getRawAxis(1), True
+        )
+
+
+# These lines should be found at the bottom and unchanged
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
---
+++ frc-docs-examples/pwm-drivetrain/robot.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    PWM Drivetrain Demo
+"""
+
+# The first lines should be the import of the needed modules
+import wpilib
+import wpilib.drive
+
+
+# All code should be found in the MyRobot class. Note that in this example, the wpilib.TimedRobot robot base is used.
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        """Robot initialization function"""
+
+        # create motor controller objects
+        m_left = wpilib.Spark(0)
+        m_right = wpilib.Spark(1)
+        self.robot_drive = wpilib.drive.DifferentialDrive(m_left, m_right)  # object that handles basic drive operations
+        self.robot_drive.setExpiration(0.1)  # Configure protection in case of code slowdown or crash
+
+        # joystick #0
+        self.stick = wpilib.Joystick(0)
+
+    def teleopInit(self):
+        """Executed once at the start of teleop mode"""
+        self.robot_drive.setSafetyEnabled(True)
+
+    def teleopPeriodic(self):
+        """Executed in loop while in teleop mode"""
+
+        # Runs the motors with tank steering
+        self.robot_drive.arcadeDrive(
+            self.stick.getRawAxis(0), self.stick.getRawAxis(1), True
+        )
+
+
+# These lines should be found at the bottom and unchanged
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
