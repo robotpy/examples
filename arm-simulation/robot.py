011918e1ffecbe6d10f6a2c3b47f35021858d979
Dustin Spicuzza <dustin@virtualroadside.com>
2023-12-03 02:43:13 -0500

    Update arm simulation example
    - Fixes #79

--- arm-simulation/robot.py
+++ arm-simulation/robot.py
@@ -4,69 +4,31 @@
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 
-import math
 import wpilib
-import wpimath.controller
+
+from subsytems.arm import Arm
+from constants import Constants
 
 
 class MyRobot(wpilib.TimedRobot):
-    kMotorPort = 0
-    kEncoderAChannel = 0
-    kEncoderBChannel = 1
-    kJoystickPort = 0
+    def robotInit(self):
+        self.arm = Arm()
+        self.joystick = wpilib.Joystick(Constants.kJoystickPort)
 
-    kArmPositionKey = "ArmPosition"
-    kArmPKey = "ArmP"
+    def teleopInit(self):
+        self.arm.loadPreferences()
 
-    # distance per pulse = (angle per revolution) / (pulses per revolution)
-    #  = (2 * PI rads) / (4096 pulses)
-    kArmEncoderDistPerPulse = 2.0 * math.pi / 4096.0
-
-    def robotInit(self) -> None:
-        # The P gain for the PID controller that drives this arm.
-        self.kArmKp = 50.0
-
-        self.armPosition = 75.0
-
-        # standard classes for controlling our arm
-        self.controller = wpimath.controller.PIDController(self.kArmKp, 0, 0)
-        self.encoder = wpilib.Encoder(self.kEncoderAChannel, self.kEncoderBChannel)
-        self.motor = wpilib.PWMSparkMax(self.kMotorPort)
-        self.joystick = wpilib.Joystick(self.kJoystickPort)
-
-        self.encoder.setDistancePerPulse(self.kArmEncoderDistPerPulse)
-
-        # Set the Arm position setpoint and P constant to Preferences if the keys
-        # don't already exist
-        if not wpilib.Preferences.containsKey(self.kArmPositionKey):
-            wpilib.Preferences.setDouble(self.kArmPositionKey, self.armPosition)
-        if not wpilib.Preferences.containsKey(self.kArmPKey):
-            wpilib.Preferences.setDouble(self.kArmPKey, self.kArmKp)
-
-    def teleopInit(self) -> None:
-        # Read Preferences for Arm setpoint and kP on entering Teleop
-        self.armPosition = wpilib.Preferences.getDouble(
-            self.kArmPositionKey, self.armPosition
-        )
-        if self.kArmKp != wpilib.Preferences.getDouble(self.kArmPKey, self.kArmKp):
-            self.kArmKp = wpilib.Preferences.getDouble(self.kArmPKey, self.kArmKp)
-            self.controller.setP(self.kArmKp)
-
-    def teleopPeriodic(self) -> None:
+    def teleopPeriodic(self):
         if self.joystick.getTrigger():
-            # Here we run PID control like normal, with a setpoint read from
-            # preferences in degrees
-            pidOutput = self.controller.calculate(
-                self.encoder.getDistance(), math.radians(self.armPosition)
-            )
-            self.motor.setVoltage(pidOutput)
+            # Here, we run PID control like normal.
+            self.arm.reachSetpoint()
         else:
-            # Otherwise we disable the motor
-            self.motor.set(0.0)
+            # Otherwise, we disable the motor.
+            self.arm.stop()
 
-    def disabledInit(self) -> None:
-        # This just makes sure that our simulation code knows that the motor is off
-        self.motor.set(0)
+    def disabledInit(self):
+        # This just makes sure that our simulation code knows that the motor's off.
+        self.arm.stop()
 
 
 if __name__ == "__main__":
