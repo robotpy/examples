0ff77598ef0da684ccd0713ffe1a0a6c8ce13264
Guilherme Samuel <guisamuel800@gmail.com>
2023-12-24 00:43:16 -0300

    add drivetrain swervemodule file

---
+++ SwerveBot/drivetrain.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+
+import math
+import wpimath.geometry
+import wpimath.kinematics
+import swervemodule
+
+kMaxSpeed = 3.0 # 3 meters per second
+kMaxAngularSpeed = math.pi # 1/2 rotation per second
+
+class Drivetrain:
+	"""
+	Represents a swerve drive style drivetrain.
+	"""
+	def __init__(self) -> None:
+		self.frontLeftLocation = wpimath.geometry.Transform2d(0.381, 0.381)
+		self.frontRightLocation = wpimath.geometry.Transform2d(0.381, -0.381)
+		self.backLeftLocation = wpimath.geometry.Transform2d(-0.381, 0.381)
+		self.backRightLocaton = wpimath.geometry.Transform2d(-0.381, -0.381)
+		
+		self.frontLeft = swervemodule.SwerveModule(1, 2, 0, 1, 2, 3)
+		self.frontRight = swervemodule.SwerveModule(3, 4, 4, 5, 6, 7)
+		self.backLeft = swervemodule.SwerveModule(5, 6, 8, 9, 10, 11)
+		self.backRight = swervemodule.SwerveModule(7, 8, 12, 13, 14, 15)
+
+		self.gyro = wpilib.AnalogGyro(0)
+
+		self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(
+			self.frontLeftLocation, self.frontRightLocation, self.backLeftLocation, self.backRightLocation
+		)
+
+		self.odometry = wpimath.kinematics.SwerveDrive4Odometry(
+			self.kinematics,
+			self.gyro.getRotation2d(),
+			( frontLeft.getPosition(), frontRight.getPosition(), backLeft.getPosition(), backRight.getPosition())
+		)
+
+		self.gyro.reset()
+	
+	def drive(self, xSpeed: float, ySpeed: float, rot: float, fieldRelative: bool, periodSeconds: float) -> None:
+		"""
+		Method to drive the robot using joystick info.
+		:param xSpeed: Speed of the robot in the x direction (forward).
+		:param ySpeed: Speed of the robot in the y direction (sideways).
+		:param rot: Angular rate of the robot.
+		:param fieldRelative: Whether the provided x and y speeds are relative to the field.
+		:param periodSeconds: 
+		"""
+		swerveModuleStates = self.kinematics.toSwerveModuleStates(
+			wpimath.kinematics.ChassisSpeeds.discretize(
+				wpilib.ChassisSpeeds.fromFieldRelativeSpeeds(
+					xSpeed, ySpeed, rot, self.gyro.getRotation2d()
+				) if fieldRelative else wpilib.ChassisSpeeds(xSpeed, ySpeed, rot),
+				periodSeconds
+			)
+		)
+		wpilib.kinematics.SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed)
+		self.frontLeft.setDesiredState(swerveModuleStates[0])
+		self.frontRight.setDesiredState(swerveModuleStates[1])
+		self.backLeft.setDesiredState(swerveModuleStates[2])
+		self.backRight.setDesiredState(swerveModuleStates[3])
+	
+	def updateOdometry(self) -> None:
+		"""Updates the field relative position of the robot."""
+		self.odometry.update(
+			self.gyro.getRotation2d(),
+			( self.frontLeft, self.frontRight, self.backLeft, self.backRight )
+		)
