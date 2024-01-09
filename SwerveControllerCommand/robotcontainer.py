f126455ef1605d45091198e82ac2745865d299a5
Ryan Abraham <ryan.ryanabraham@gmail.com>
2024-01-09 15:49:57 -0800

    Converted header docstrings into comments

--- SwerveControllerCommand/robotcontainer.py
+++ SwerveControllerCommand/robotcontainer.py
@@ -1,8 +1,8 @@
-"""
-Copyright (c) FIRST and other WPILib contributors.
-Open Source Software; you can modify and/or share it under the terms of
-the WPILib BSD license file in the root directory of this project.
-"""
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
 
 from wpilib import XboxController
 import commands2
