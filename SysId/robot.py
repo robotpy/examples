108bd04566f9e3d35a8cc4b1b3204388e7ce447d
Lucien Morey <lucienp@live.com.au>
2024-01-20 14:03:34 +1100

    make robot inherit from timed command robot instead

--- SysId/robot.py
+++ SysId/robot.py
@@ -4,15 +4,12 @@
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 
-from commands2 import CommandScheduler
-
-import wpilib
-
+from commands2 import CommandScheduler, TimedCommandRobot
 
 from sysidroutinebot import SysIdRoutineBot
 
 
-class MyRobot(wpilib.TimedRobot):
+class MyRobot(TimedCommandRobot):
     """The VM is configured to automatically run this class, and to call the functions corresponding to
     each mode, as described in the TimedRobot documentation. If you change the name of this class or
     the package after creating this project, you must also update the build.gradle file in the
@@ -28,20 +25,6 @@ class MyRobot(wpilib.TimedRobot):
         self.robot.configureBindings()
 
         self.autonomous_command = self.robot.getAutonomousCommand()
-
-    def robotPeriodic(self) -> None:
-        """This function is called every robot packet, no matter the mode. Use this for items like
-        diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-
-        This runs after the mode specific periodic functions, but before LiveWindow and
-        SmartDashboard integrated updating.
-        """
-
-        # Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        # commands, running already-scheduled commands, removing finished or interrupted commands,
-        # and running subsystem periodic() methods.  This must be called from the robot's periodic
-        # block in order for anything in the Command-based framework to work.
-        CommandScheduler.getInstance().run()
 
     def disabledInit(self) -> None:
         """This function is called once each time the robot enters Disabled mode."""
